2d5df7ac16908c6a6cf183a5b958339d41b085af
Marco Galindo <marcodavidg@gmail.com>
2016-04-03 00:15:33 -0600

    Added files via upload

---
+++ VB/Compi2_Proyecto1_1s2016/Compi2_Proyecto1_1s2016/Form1.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripDropDownButton1 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.SfasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GdToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LimpiarTextoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GuardarDocumentoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.BuscarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BuscarYReemplazarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripDropDownButton2 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.ProbarConexionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EnvianEntradaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.returnText = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ToolStripSeparator4 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripTextBox1 = New System.Windows.Forms.ToolStripTextBox()
+        Me.ToolStripButton3 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripTextBox2 = New System.Windows.Forms.ToolStripTextBox()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripDropDownButton1, Me.ToolStripSeparator1, Me.ToolStripButton1, Me.ToolStripSeparator3, Me.ToolStripDropDownButton2, Me.ToolStripSeparator4, Me.ToolStripButton2, Me.ToolStripTextBox1, Me.ToolStripButton3, Me.ToolStripTextBox2})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(704, 25)
+        Me.ToolStrip1.TabIndex = 4
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripDropDownButton1
+        '
+        Me.ToolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripDropDownButton1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SfasToolStripMenuItem, Me.GdToolStripMenuItem, Me.LimpiarTextoToolStripMenuItem, Me.GuardarDocumentoToolStripMenuItem, Me.ToolStripSeparator2, Me.BuscarToolStripMenuItem, Me.BuscarYReemplazarToolStripMenuItem})
+        Me.ToolStripDropDownButton1.Image = CType(resources.GetObject("ToolStripDropDownButton1.Image"), System.Drawing.Image)
+        Me.ToolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripDropDownButton1.Name = "ToolStripDropDownButton1"
+        Me.ToolStripDropDownButton1.Size = New System.Drawing.Size(61, 22)
+        Me.ToolStripDropDownButton1.Text = "Archivo"
+        '
+        'SfasToolStripMenuItem
+        '
+        Me.SfasToolStripMenuItem.Name = "SfasToolStripMenuItem"
+        Me.SfasToolStripMenuItem.Size = New System.Drawing.Size(191, 22)
+        Me.SfasToolStripMenuItem.Text = "Nueva Pestaña"
+        '
+        'GdToolStripMenuItem
+        '
+        Me.GdToolStripMenuItem.Name = "GdToolStripMenuItem"
+        Me.GdToolStripMenuItem.Size = New System.Drawing.Size(191, 22)
+        Me.GdToolStripMenuItem.Text = "Abrir Documento"
+        '
+        'LimpiarTextoToolStripMenuItem
+        '
+        Me.LimpiarTextoToolStripMenuItem.Name = "LimpiarTextoToolStripMenuItem"
+        Me.LimpiarTextoToolStripMenuItem.Size = New System.Drawing.Size(191, 22)
+        Me.LimpiarTextoToolStripMenuItem.Text = "Limpiar Texto"
+        '
+        'GuardarDocumentoToolStripMenuItem
+        '
+        Me.GuardarDocumentoToolStripMenuItem.Name = "GuardarDocumentoToolStripMenuItem"
+        Me.GuardarDocumentoToolStripMenuItem.Size = New System.Drawing.Size(191, 22)
+        Me.GuardarDocumentoToolStripMenuItem.Text = "Guardar Documento"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(188, 6)
+        '
+        'BuscarToolStripMenuItem
+        '
+        Me.BuscarToolStripMenuItem.Name = "BuscarToolStripMenuItem"
+        Me.BuscarToolStripMenuItem.Size = New System.Drawing.Size(191, 22)
+        Me.BuscarToolStripMenuItem.Text = "Buscar..."
+        '
+        'BuscarYReemplazarToolStripMenuItem
+        '
+        Me.BuscarYReemplazarToolStripMenuItem.Name = "BuscarYReemplazarToolStripMenuItem"
+        Me.BuscarYReemplazarToolStripMenuItem.Size = New System.Drawing.Size(191, 22)
+        Me.BuscarYReemplazarToolStripMenuItem.Text = "Buscar y Reemplazar..."
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripDropDownButton2
+        '
+        Me.ToolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripDropDownButton2.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ProbarConexionToolStripMenuItem, Me.EnvianEntradaToolStripMenuItem})
+        Me.ToolStripDropDownButton2.Image = CType(resources.GetObject("ToolStripDropDownButton2.Image"), System.Drawing.Image)
+        Me.ToolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripDropDownButton2.Name = "ToolStripDropDownButton2"
+        Me.ToolStripDropDownButton2.Size = New System.Drawing.Size(70, 22)
+        Me.ToolStripDropDownButton2.Text = "Conexion"
+        '
+        'ProbarConexionToolStripMenuItem
+        '
+        Me.ProbarConexionToolStripMenuItem.Name = "ProbarConexionToolStripMenuItem"
+        Me.ProbarConexionToolStripMenuItem.Size = New System.Drawing.Size(162, 22)
+        Me.ProbarConexionToolStripMenuItem.Text = "Probar Conexion"
+        '
+        'EnvianEntradaToolStripMenuItem
+        '
+        Me.EnvianEntradaToolStripMenuItem.Name = "EnvianEntradaToolStripMenuItem"
+        Me.EnvianEntradaToolStripMenuItem.Size = New System.Drawing.Size(162, 22)
+        Me.EnvianEntradaToolStripMenuItem.Text = "Enviar Entrada"
+        '
+        'ToolStripSeparator3
+        '
+        Me.ToolStripSeparator3.Name = "ToolStripSeparator3"
+        Me.ToolStripSeparator3.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton1.Image = CType(resources.GetObject("ToolStripButton1.Image"), System.Drawing.Image)
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(148, 22)
+        Me.ToolStripButton1.Text = "Guardar Resultados en .txt"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.TabControl1.Location = New System.Drawing.Point(0, 25)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(704, 293)
+        Me.TabControl1.TabIndex = 5
+        '
+        'returnText
+        '
+        Me.returnText.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.returnText.Location = New System.Drawing.Point(0, 356)
+        Me.returnText.Name = "returnText"
+        Me.returnText.Size = New System.Drawing.Size(704, 167)
+        Me.returnText.TabIndex = 6
+        Me.returnText.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("SF Atarian System", 20.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(286, 329)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(124, 24)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "RESULTADOS"
+        '
+        'ToolStripSeparator4
+        '
+        Me.ToolStripSeparator4.Name = "ToolStripSeparator4"
+        Me.ToolStripSeparator4.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton2.Image = CType(resources.GetObject("ToolStripButton2.Image"), System.Drawing.Image)
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(46, 22)
+        Me.ToolStripButton2.Text = "Buscar"
+        '
+        'ToolStripTextBox1
+        '
+        Me.ToolStripTextBox1.Name = "ToolStripTextBox1"
+        Me.ToolStripTextBox1.Size = New System.Drawing.Size(100, 25)
+        '
+        'ToolStripButton3
+        '
+        Me.ToolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.ToolStripButton3.Image = CType(resources.GetObject("ToolStripButton3.Image"), System.Drawing.Image)
+        Me.ToolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton3.Name = "ToolStripButton3"
+        Me.ToolStripButton3.Size = New System.Drawing.Size(81, 22)
+        Me.ToolStripButton3.Text = "y Reemplazar"
+        '
+        'ToolStripTextBox2
+        '
+        Me.ToolStripTextBox2.Name = "ToolStripTextBox2"
+        Me.ToolStripTextBox2.Size = New System.Drawing.Size(100, 25)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(704, 523)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.returnText)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ToolStripDropDownButton1 As System.Windows.Forms.ToolStripDropDownButton
+    Friend WithEvents SfasToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents GdToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LimpiarTextoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents GuardarDocumentoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents BuscarToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BuscarYReemplazarToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripDropDownButton2 As System.Windows.Forms.ToolStripDropDownButton
+    Friend WithEvents ProbarConexionToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EnvianEntradaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents returnText As System.Windows.Forms.RichTextBox
+    Friend WithEvents ToolStripSeparator3 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ToolStripButton1 As System.Windows.Forms.ToolStripButton
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ToolStripSeparator4 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ToolStripButton2 As System.Windows.Forms.ToolStripButton
+    Friend WithEvents ToolStripTextBox1 As System.Windows.Forms.ToolStripTextBox
+    Friend WithEvents ToolStripButton3 As System.Windows.Forms.ToolStripButton
+    Friend WithEvents ToolStripTextBox2 As System.Windows.Forms.ToolStripTextBox
+
+End Class
---
+++ VB/Compi2_Proyecto1_1s2016/Compi2_Proyecto1_1s2016/Form1.vb
@@ -0,0 +1,224 @@
+﻿Imports System.Net.Sockets
+Imports System.IO
+
+Public Class Form1
+    Dim punteroText As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Shared Sub Connect(ByVal serverIP As String, ByVal message As String, ByVal tipoRequest As Int32)
+        Dim output As String = ""
+        message = message & Chr(tipoRequest)
+        Try
+            ' Create a TcpClient. 
+            ' The client requires a TcpServer that is connected 
+            ' to the same address specified by the server and port 
+            ' combination. 
+            Dim port As Int32 = 19999
+            Dim client As New TcpClient(serverIP, port)
+
+            ' Translate the passed message into ASCII and store it as a byte array. 
+            Dim data(255) As [Byte]
+            data = System.Text.Encoding.ASCII.GetBytes(message)
+
+            ' Get a client stream for reading and writing. 
+            ' Stream stream = client.GetStream(); 
+            Dim stream As NetworkStream = client.GetStream()
+
+            ' Send the message to the connected TcpServer. 
+            stream.Write(data, 0, data.Length)
+
+            ' Buffer to store the response bytes.
+            data = New [Byte](255) {}
+
+            ' String to store the response ASCII representation. 
+            Dim responseData As String = String.Empty
+
+            ' Read the first batch of the TcpServer response bytes. 
+            Dim bytes As Int32 = stream.Read(data, 0, data.Length)
+            responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes)
+            MessageBox.Show(responseData)
+            Form1.returnText.Text = responseData
+            ' Close everything.
+            stream.Close()
+            client.Close()
+        Catch e As ArgumentNullException
+            output = "ArgumentNullException: " + e.ToString()
+            Form1.returnText.Text = output
+        Catch e As SocketException
+            output = "SocketException: " + e.ToString()
+            MessageBox.Show(output)
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ToolStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles ToolStrip1.ItemClicked
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        newTab()
+        punteroText = 0
+    End Sub
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Sub newTab()
+        Dim richtext As New RichTextBox
+        Dim edittab As New TabPage
+        Dim lines As New LineNumbers_For_RichTextBox
+        edittab.BackColor = Color.White
+        richtext.Width = 650
+        richtext.Height = 218
+        richtext.Name = "RTB"
+        richtext.Dock = DockStyle.Right
+        lines.BackColor = Color.White
+        lines.ParentRichTextBox = richtext
+        lines.Dock = DockStyle.Left
+        lines.Width = 20
+        lines.Height = 218
+        edittab.Text = "New Tab"
+        edittab.Controls.Add(lines)
+        edittab.Controls.Add(richtext)
+        TabControl1.TabPages.Add(edittab)
+        TabControl1.SelectTab(edittab)
+    End Sub
+
+    Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ToolStripButton4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub ToolStripDropDownButton1_Click(sender As Object, e As EventArgs) Handles ToolStripDropDownButton1.Click
+
+    End Sub
+
+    Private Sub BuscarYReemplazarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BuscarYReemplazarToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub SfasToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SfasToolStripMenuItem.Click
+        newTab()
+    End Sub
+
+    Private Sub GdToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GdToolStripMenuItem.Click
+        Dim txt As String
+        OpenFileDialog1.Filter = "Text File | *.txt"
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            txt = OpenFileDialog1.FileName
+            Dim fileReader As String
+            fileReader = My.Computer.FileSystem.ReadAllText(txt)
+            TabControl1.SelectedTab.Controls("RTB").Text = fileReader
+        End If
+    End Sub
+
+    Private Sub ToolStripButton3_Click(sender As Object, e As EventArgs)
+    End Sub
+
+    Private Sub LimpiarTextoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LimpiarTextoToolStripMenuItem.Click
+        TabControl1.SelectedTab.Controls("RTB").Text = ""
+    End Sub
+
+    Private Sub EnvianEntradaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EnvianEntradaToolStripMenuItem.Click
+        Dim entrada As String
+        entrada = TabControl1.SelectedTab.Controls("RTB").Text
+        ' In this code example, use a hard-coded 
+        ' IP address and message. 
+        Dim serverIP As String = "localhost"
+        Dim message As String = "Calling the Socket Server on localhost port 19999 at " + DateTime.Now
+        Connect(serverIP, entrada, 1)
+    End Sub
+
+    Private Sub ProbarConexionToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ProbarConexionToolStripMenuItem.Click
+        ' In this code example, use a hard-coded 
+        ' IP address and message. 
+        Dim serverIP As String = "localhost"
+        Dim message As String = "Calling the Socket Server on localhost port 19999 at " + DateTime.Now
+        Connect(serverIP, message, 13)
+    End Sub
+
+    Private Sub GuardarDocumentoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GuardarDocumentoToolStripMenuItem.Click
+        saveFileDialog1.Filter = "Text File|*.txt"
+        SaveFileDialog1.ShowDialog()
+        Dim path As String
+        path = SaveFileDialog1.FileName
+        If path <> "" Then
+            MsgBox(path)
+            File.Create(path).Dispose()
+            Dim entrada As String
+            entrada = TabControl1.SelectedTab.Controls("RTB").Text
+            My.Computer.FileSystem.WriteAllText(path, entrada, False)
+        End If
+        
+    End Sub
+
+    Private Sub ToolStripButton1_Click_1(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+        SaveFileDialog1.Filter = "Text File|*.txt"
+        SaveFileDialog1.ShowDialog()
+        Dim path As String
+        path = SaveFileDialog1.FileName
+        If path <> "" Then
+            MsgBox(path)
+            File.Create(path).Dispose()
+            My.Computer.FileSystem.WriteAllText(path, returnText.Text, False)
+        End If
+
+    End Sub
+
+    Private Sub ToolStripButton3_Click_1(sender As Object, e As EventArgs) Handles ToolStripButton3.Click
+        Dim old As String = ToolStripTextBox1.Text
+        Dim replacetext As String = ToolStripTextBox2.Text
+        If TabControl1.SelectedTab.Controls("RTB").Text.Length > 0 Then
+            TabControl1.SelectedTab.Controls("RTB").Text = TabControl1.SelectedTab.Controls("RTB").Text.Replace(old, replacetext)
+        End If
+    End Sub
+
+    Private Sub ToolStripButton2_Click_1(sender As Object, e As EventArgs) Handles ToolStripButton2.Click
+        Dim old As String = ToolStripTextBox1.Text
+        Dim a As RichTextBox
+        Dim encontrado As Integer
+        encontrado = 818000
+        a = TabControl1.SelectedTab.Controls("RTB")
+        If TabControl1.SelectedTab.Controls("RTB").Text.Length > 0 Then
+            a.Select(punteroText, a.Text.Length)
+            If punteroText >= a.Text.Length Then
+                punteroText = 0
+            Else
+                encontrado = a.Find(old, punteroText, RichTextBoxFinds.MatchCase)
+                If encontrado < 818000 Then
+                    punteroText = old.Length + a.SelectionStart
+                Else
+                    encontrado = 818000
+                    punteroText = 0
+                    encontrado = a.Find(old, punteroText, RichTextBoxFinds.MatchCase)
+                    If encontrado = 818000 Then
+                        a.SelectedText = "zq6efasdf"
+                        punteroText = 0
+                    Else
+                        punteroText = old.Length + a.SelectionStart
+                    End If
+                End If
+            End If
+
+        End If
+    End Sub
+
+    Private Sub ToolStripTextBox1_Click(sender As Object, e As EventArgs) Handles ToolStripTextBox1.Click
+
+    End Sub
+
+    Private Sub ToolStripTextBox1_TextChanged(sender As Object, e As EventArgs) Handles ToolStripTextBox1.TextChanged
+        punteroText = 0
+    End Sub
+End Class
